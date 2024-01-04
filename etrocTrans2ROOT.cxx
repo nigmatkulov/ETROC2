fe818f855aabe2864b5108eb139e39702bb9746b
Grigory Nigmatkulov <nigmatkulov@gmail.com>
2024-01-04 12:16:47 -0600

    First working version

--- etrocTrans2ROOT.cxx
+++ etrocTrans2ROOT.cxx
@@ -15,19 +15,24 @@
 #include "Hit.h"
 
 //________________
+void readInputFile(const Char_t* inFileName, TTree *T = nullptr);
+
+//________________
 int main(int argc, char const *argv[]) {
 
     const Char_t *inFileName="/Users/gnigmat/work/cms/data/etroc/Run_2_loop_1/TDC_Data_translated_0.nem";
     const Char_t *oFileName = "oEtroc2Trans.root";
 
+    if (argc < 2) {
+        std::cerr << "Usage: " << argv[0] << " <inputFileName>" << std::endl;
+        return 1;
+    }
+
     // Data structure
     UInt_t runId{123};
     UInt_t loopId{1};
     UInt_t eventId{0};
-    std::vector<Hit> board0;
-    std::vector<Hit> board1;
-    std::vector<Hit> board2;
-    std::vector<Hit> board3;
+    std::vector<Hit> Hits;
 
     Hit::Class()->IgnoreTObjectStreamer();
 
@@ -37,12 +42,47 @@ int main(int argc, char const *argv[]) {
     T->Branch("mRunId",   &runId);
     T->Branch("mLoopId",  &loopId);
     T->Branch("mEventId", &eventId);
-    
-    T->Branch("mBoard0", &board0);
-    T->Branch("mBoard1", &board1);
-    T->Branch("mBoard2", &board2);
-    T->Branch("mBoard3", &board3);
+    T->Branch("mHits", &Hits);
 
+    // Read input argument list 
+    if (argc > 1) inFileName = argv[1];
+    if (argc > 2) oFileName = argv[2];
+
+    // Check if the filename ends with ".list"
+    if (inFileName.length() >= 5 && inFileName.substr(filename.length() - 5) == ".list") {
+        std::cout << "The filename ends with \".list\"" << std::endl;
+    } 
+    else if (inFileName.length() >= 4 && inFileName.substr(filename.length() - 4) == ".nem") {
+        // Check if the filename ends with ".nem"
+        std::cout << "The filename ends with \".nem\"" << std::endl;
+        readInputFile(inFileName, T);
+    } 
+    else {
+        std::cout << "The filename does not end with \".list\" or \".nem\"" << std::endl;
+    }
+
+    // Open the input file
+    std::ifstream inputFile("inputFiles.list");
+
+    if (!inputFile.is_open()) {
+        std::cerr << "Error opening the input file." << std::endl;
+        return 1;
+    }
+
+    // Regular expression to match the pattern "Run_<number>_loop_<number>"
+    std::regex pattern("Run_(\\d+)_loop_(\\d+)");
+
+    inputFile.close();
+
+
+
+    return 0;
+}
+
+//________________
+void readInputFile(const Char_t* inFileName, TTree *T = nullptr) {
+
+    std::cout << Form("Read input file: %s\n", inFileName);
     std::ifstream inputFile(inFileName); 
 
     if (!inputFile.is_open()) {
@@ -95,24 +135,7 @@ int main(int argc, char const *argv[]) {
             hit.setCal(cal);
 
             // Add hit to a proper board
-            switch (boardNumber)
-            {
-            case 0:
-                board0.push_back(hit);
-                break;
-            case 1:
-                board1.push_back(hit);
-                break;
-            case 2:
-                board2.push_back(hit);
-                break;
-            case 3:
-                board3.push_back(hit);
-                break;
-            default:
-                std::cout << Form("Wrong board number: %d\n", boardNumber);
-                break;
-            }
+            Hits.push_back( hit );
         }
         else if (line.substr(0, 2) == "ET") {
 
@@ -123,10 +146,7 @@ int main(int argc, char const *argv[]) {
             // }
 
             T->Fill();
-            board0.clear();
-            board1.clear();
-            board2.clear();
-            board3.clear();
+            Hits.clear();
             // Increment event counter
             eventId++;
         } 
@@ -135,8 +155,6 @@ int main(int argc, char const *argv[]) {
         }
     } // while (inputFile)
     std::cout << "\t[DONE]" << std::endl;
-
-    inputFile.close();
 
     // Create a ROOT file to store the TTree
     TFile *oFile = new TFile(oFileName, "RECREATE");
--- readEtroc2Boards.cxx
+++ readEtroc2Boards.cxx
@@ -13,6 +13,7 @@
 #include "TH2D.h"
 #include "TSystem.h"
 #include "TCanvas.h"
+#include "TStyle.h"
 
 // C++ headers
 #include <iostream>
@@ -28,11 +29,85 @@ Int_t status = gSystem->Load("/Users/gnigmat/work/cms/soft/etroc/build/libETROC2
 #endif
 
 //________________
+void set1DStyle(TH1 *h, Int_t type = 0) {
+    // type: 0 - reco, 1 - gen, 2 - ref
+    Int_t markerStyle = 20; // Full circle
+    Double_t markerSize = 1.2;
+    Int_t lineWidth{2}; 
+    Int_t color{2};
+    if (type == 0) {
+
+    }
+    else if (type == 1) { // gen
+        color = 4;
+        markerStyle = 24;
+    }
+    else if (type == 2) { // ref
+        color = 8;
+        markerStyle = 33;
+        markerSize = 1.5;
+    }
+    else if (type == 3) {
+        color = 6;
+        markerStyle = 35;
+        markerSize = 1.3;
+    }
+    else if ( type == 4 ) {
+        color = 7;
+        markerStyle = 37;
+        markerSize = 1.3;  
+    }
+    else if ( type == 5 ) {
+        color = 11;
+        markerStyle = 40;
+        markerSize = 1.3;  
+    }
+    else if ( type == 6 ) {
+        color = 15;
+        markerStyle = 41;
+        markerSize = 1.3;  
+    }
+    else {
+        color = 9;
+        markerStyle = 38;
+        markerSize = 1.3;
+    }
+
+    h->SetLineWidth( lineWidth );
+    h->SetLineColor( color );
+    
+    h->SetMarkerStyle( markerStyle );
+    h->SetMarkerColor( color );
+    h->SetMarkerSize( markerSize );
+
+    h->GetYaxis()->SetTitleSize(0.06);
+    h->GetYaxis()->SetLabelSize(0.06);
+    h->GetXaxis()->SetTitleSize(0.06);
+    h->GetXaxis()->SetLabelSize(0.06);
+    h->GetXaxis()->SetNdivisions(208);
+    h->GetYaxis()->SetNdivisions(208);    
+    h->GetYaxis()->SetTitleOffset(1.0);
+}
+
+//________________
+void set2DStyle(TH2* h) {
+    h->GetYaxis()->SetTitleSize(0.06);
+    h->GetYaxis()->SetLabelSize(0.06);
+    h->GetXaxis()->SetTitleSize(0.06);
+    h->GetXaxis()->SetLabelSize(0.06);
+    h->GetXaxis()->SetNdivisions(208);
+    h->GetYaxis()->SetNdivisions(208);    
+    h->GetYaxis()->SetTitleOffset(1.0);
+}
+
+//________________
 int main(int argc, char const *argv[]) {
 
 //int readEtroc2Boards() {
 
-    const Char_t *inFileName = "/Users/gnigmat/work/cms/soft/etroc/build/oEtroc2Trans.root";
+    gStyle->SetPalette(kBird);
+
+    const Char_t *inFileName = "/Users/gnigmat/work/cms/soft/ETROC2/build/oEtroc2Trans.root";
     
     TFile *inFile = TFile::Open(inFileName);
 
@@ -45,7 +120,6 @@ int main(int argc, char const *argv[]) {
     // Histograms
     //
 
-    //TCanvas *canv = new TCanvas("canv","canv", 1200, 800);
     auto *h1 = new TH1D("h1", "h;TOT;Entries", 50, 0., 500.);
     h1->Sumw2();
     auto *h2 = new TH1D("h2", "Event", 10000, 0., 10000);
@@ -59,12 +133,15 @@ int main(int argc, char const *argv[]) {
         hNHits[i] = new TH1D(Form("hNHits_%d", i), 
                              Form("Number of hits in board #%d", i),
                              10, -0.5, 9.5);
+        set1DStyle(hNHits[i], i);
         hTOT[i] = new TH1D(Form("hTOT_%d", i), 
                            Form("TOT for the board #%d", i),
                            500, 0, 1500);
+        set1DStyle(hTOT[i], i);
         hTOTvsTOA[i] = new TH2D(Form("hTOTvsTOA_%d",i), 
                                 Form("TOT vs. TOA for board #%d;TOA;TOT",i), 
                                 512, 0., 1024., 512, 0., 512.);
+        set2DStyle(hTOTvsTOA[i]);
     }
 
     //
@@ -74,11 +151,11 @@ int main(int argc, char const *argv[]) {
     UInt_t runId;
     UInt_t loopId;
     UInt_t eventId;
+    std::vector<Hit> *mHits{nullptr};
 
-    std::vector<Hit> *mBoard0{nullptr};
-    std::vector<Hit> *mBoard1{nullptr};
-    std::vector<Hit> *mBoard2{nullptr};
-    std::vector<Hit> *mBoard3{nullptr};
+    //
+    // Retrieve a tree
+    //
 
     auto T = dynamic_cast<TTree*>( inFile->Get("T") );
     if ( !T ) {
@@ -92,16 +169,19 @@ int main(int argc, char const *argv[]) {
     //
 
     T->SetBranchAddress( "mEventId", &eventId );
+    T->SetBranchAddress( "mHits", &mHits );
 
-    T->SetBranchAddress( "mBoard0", &mBoard0 );
-    T->SetBranchAddress( "mBoard1", &mBoard1 );
-    T->SetBranchAddress( "mBoard2", &mBoard2 );
-    T->SetBranchAddress( "mBoard3", &mBoard3 );
+    //
+    // Retrieve number of events to analyze and create variables
+    //
 
-    // Retrieve number of events
     Long64_t nEntries = T->GetEntries();
+    Int_t boardId{0};
 
-    // Loop over event in TTree
+    //
+    // Loop over event in TTree and do analysis
+    //
+
     for (Long64_t iEntry{0}; iEntry < nEntries; ++iEntry) {
 
         // Load the entry from the tree
@@ -109,38 +189,29 @@ int main(int argc, char const *argv[]) {
 
         h2->Fill( eventId );
 
-        if ( !mBoard0->empty() ) hNHits[0]->Fill( mBoard0->size() );
-        if ( !mBoard1->empty() ) hNHits[1]->Fill( mBoard1->size() );
-        if ( !mBoard2->empty() ) hNHits[2]->Fill( mBoard2->size() );
-        if ( !mBoard3->empty() ) hNHits[3]->Fill( mBoard3->size() );
+        Int_t mNumberOfHitsPerBoard[4] = {0, 0, 0, 0};
 
         // Loop over hits in each board
-        for (UInt_t iHit{0}; iHit<mBoard0->size(); iHit++) {
-            Hit hit = mBoard0->at(iHit);
-            //if (!hit) continue;
-            hTOT[0]->Fill( hit.tot() );
-            hTOTvsTOA[0]->Fill( hit.toa(), hit.tot() );
-        }
-        for (UInt_t iHit{0}; iHit<mBoard1->size(); iHit++) {
-            Hit hit = mBoard1->at(iHit);
-            //if (!hit) continue;
-            hTOT[1]->Fill( hit.tot() );
-            hTOTvsTOA[1]->Fill( hit.toa(), hit.tot() );
-        }
-        for (UInt_t iHit{0}; iHit<mBoard2->size(); iHit++) {
-            Hit hit = mBoard2->at(iHit);
-            //if (!hit) continue;
-            hTOT[2]->Fill( hit.tot() );
-            hTOTvsTOA[2]->Fill( hit.toa(), hit.tot() );
-        }
-        for (UInt_t iHit{0}; iHit<mBoard3->size(); iHit++) {
-            Hit hit = mBoard3->at(iHit);
-            //if (!hit) continue;
-            hTOT[3]->Fill( hit.tot() );
-            hTOTvsTOA[3]->Fill( hit.toa(), hit.tot() );
+        for (UInt_t iHit{0}; iHit<mHits->size(); iHit++) {
+
+            // Retrieve the hit
+            Hit hit = mHits->at( iHit );
+
+            boardId = hit.boardNumber();
+
+            // For the setups that do not contain more than 4 boards
+            if ( TMath::Abs(boardId) > 4 ) continue;
+
+            // Increment hit counter (per board)
+            mNumberOfHitsPerBoard[ boardId ]++;
+            hTOT[ boardId ]->Fill( hit.tot() );
+            hTOTvsTOA[ boardId ]->Fill( hit.toa(), hit.tot() );
         }
 
-        
+        // Loop over boards to fill number of hits per board
+        for (Int_t iBoard{0}; iBoard<4; iBoard++) {
+            hNHits[iBoard]->Fill( mNumberOfHitsPerBoard[iBoard] );
+        }     
     }
 
     TFile *oFile = new TFile("oReadEtroc2.root", "recreate");
